c9ccb1c93e1589a159b1dbc3861a265270aa1444
GU YI <yguax@connect.ust.hk>
2022-07-15 23:18:13 +0800

    Protocl

---
+++ app/src/main/java/com/example/uropproject/DKeyGen.java
@@ -0,0 +1,45 @@
+package com.example.uropproject;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import org.javatuples.Triplet;
+import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.math.ec.ECCurve;
+
+import javafx.util.Pair;
+
+public class DKeyGen {
+	private ArrayList<Pair<BigInteger, BigInteger>> msk;
+	private ArrayList<BigInteger> w;
+	private Triplet<ArrayList<BigInteger>,ECPoint,ECPoint> fpk;
+	private BigInteger p;
+	BigInteger ws_1=BigInteger.ZERO;
+	BigInteger ws_2=BigInteger.ZERO;
+	public DKeyGen(ArrayList<Pair<BigInteger, BigInteger>> msk,ArrayList<BigInteger> w,BigInteger p) {
+		this.msk=msk;
+		this.w=w;
+		this.p=p;
+
+		// TODO Auto-generated constructor stub
+	}
+	public Pair<BigInteger, BigInteger> getfsk(){
+		ws_1=BigInteger.ZERO;
+		ws_2=BigInteger.ZERO;
+		for(int i=0;i<w.size();i++) {
+			ws_1=(ws_1.add(w.get(i).multiply(msk.get(i).getKey()))).mod(p);
+			ws_2=(ws_2.add(w.get(i).multiply(msk.get(i).getValue()))).mod(p);
+		}
+		System.out.println("fsk is");
+		System.out.println(ws_1);
+		System.out.println(ws_2);
+		return new Pair<BigInteger, BigInteger>(ws_1, ws_2);
+
+	}
+	public Triplet<ArrayList<BigInteger>,ECPoint,ECPoint> getfpk(ECPoint G,ECCurve curve) {
+		getfsk();
+		fpk=new Triplet<ArrayList<BigInteger>, ECPoint, ECPoint>(w,G.multiply(ws_1).normalize(),G.multiply(ws_2).normalize());
+		return fpk;
+
+	}
+
+}
---
+++ app/src/main/java/com/example/uropproject/Encryption.java
@@ -0,0 +1,49 @@
+package com.example.uropproject;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.math.ec.ECCurve;
+import javafx.util.Pair;
+
+public class Encryption {
+	private BigInteger originalmessage;
+	private BigInteger label;
+	private Pair<ECPoint, ECPoint> ut;
+	private Pair<BigInteger, BigInteger> encryptionkey;
+	private ECPoint G;
+	private ECCurve curve;
+
+	public Encryption(BigInteger x, BigInteger label, BigInteger s_1, BigInteger s_2,ECPoint G,ECCurve curve) {
+		originalmessage = x;
+		this.label = label;
+		encryptionkey = new Pair<BigInteger, BigInteger>(s_1, s_2);
+		this.G=G;
+		this.curve=curve;
+
+
+	}
+	public Pair<ECPoint, ECPoint> getut() throws NoSuchAlgorithmException {
+
+//		BigInteger h1=testUROP.nextRandomBigInteger(testUROP.securityParameter);
+		BigInteger h1=SHA256Calculator.doSHA256(label);
+		BigInteger h2=SHA256Calculator.doSHA256(h1);
+		ut=new Pair<ECPoint, ECPoint>(G.multiply(h1).normalize(), G.multiply(h2).normalize());
+		return ut;
+	}
+
+	public ECPoint getCipherText() throws NoSuchAlgorithmException {
+
+
+		ut=getut();
+		ECPoint p1=ut.getKey().multiply(encryptionkey.getKey());
+		ECPoint p2=ut.getValue().multiply(encryptionkey.getValue());
+		ECPoint p3=G.multiply(originalmessage);
+		ECPoint p4=p2.add(p3);
+		ECPoint finalEcPoint=p1.add(p4);
+		return finalEcPoint.normalize();
+	}
+
+
+
+
+}
